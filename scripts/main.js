507356617304fc81466cc3cb56b830c7489f800b
BlueWolf <61173114+BlueWolf3682@users.noreply.github.com>
2021-01-06 16:26:30 -0700

    Update main.js

--- scripts/main.js
+++ scripts/main.js
@@ -4,5 +4,6 @@ var lastStand = extend(SectorPreset, "last-stand", Planets.sserpulo, 69, {
   localizedName: "Last Stand",
   difficulty: 25,
   captureWave: 150,
-  alwaysUnlocked: true
+  alwaysUnlocked: true,
+  research: "ground-zero"
 });
