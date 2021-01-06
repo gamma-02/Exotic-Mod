6b976be9ff2203bfaf8eea72cf5ed9e10165e5a4
BlueWolf <61173114+BlueWolf3682@users.noreply.github.com>
2021-01-06 16:05:34 -0700

    script testing

---
+++ scripts/main.js
@@ -0,0 +1,8 @@
+require("units/hellfire")
+
+var lastStand = extend(SectorPreset, "last-stand", Planets.sun, 14, {
+  localizedName: "Last Stand",
+  difficulty: 25,
+  captureWave: 150,
+  alwaysUnlocked: true
+});
