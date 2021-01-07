af7f143f70112119f8d5ce0beec53895e834e517
BlueWolf <61173114+BlueWolf3682@users.noreply.github.com>
2021-01-06 22:14:50 -0700

    typo.

--- scripts/units/hellfire.js
+++ scripts/units/hellfire.js
@@ -1,6 +1,6 @@
 const hellfire = extendContent(UnitType, "hellfire", {});
 hellfire.constructor = () => extend(UnitEntity, {
-  removed(){
+  remove(){
     print("Cease.");
 }});
 hellfire.abilities.add(new ForceFieldAbility(300, 1450, 2546330, 25));
