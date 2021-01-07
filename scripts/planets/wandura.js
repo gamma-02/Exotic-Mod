1e1d4807444dd92340d3e3ae1ac220a640ba4fa8
BlueWolf <61173114+BlueWolf3682@users.noreply.github.com>
2021-01-07 14:50:20 -0700

    Update wandura.js

--- scripts/planets/wandura.js
+++ scripts/planets/wandura.js
@@ -1,10 +1,10 @@
 const wandura = extend(Planet, "wandura", Planets.sun, 45, 1.8, {
-  localizedName: "Wandura",
   generator: new SerpuloPlanetGenerator();
   bloom: true,
   radius: 5,
   accessible: true, 
-  hasAtmosphere: true
-  atmosphereColor: Color.valueOf("ff0000");
+  hasAtmosphere: true,
+  atmosphereColor: Color.valueOf("ff0000"),
+  localizedName: "Wandura"
 });
 wandura.meshLoader = () => extend(HexMesh, wandura, 0.8, {});
