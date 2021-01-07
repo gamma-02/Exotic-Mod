07d8b2d72ce47afff07879572c25d0c18f6bb896
BlueWolf <61173114+BlueWolf3682@users.noreply.github.com>
2021-01-07 15:54:59 -0700

    sector level 4

--- scripts/planets/wandura.js
+++ scripts/planets/wandura.js
@@ -1,4 +1,4 @@
-const wandura = extend(Planet, "wandura", Planets.sun, 2, 0.5, {
+const wandura = extend(Planet, "wandura", Planets.sun, 4, 1, {
   localizedName: "Wandura",
   generator: new SerpuloPlanetGenerator(),
   bloom: true,
