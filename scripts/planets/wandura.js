d2a868f6956e7ecdaea095dab433866c6997a1fc
BlueWolf <61173114+BlueWolf3682@users.noreply.github.com>
2021-01-07 15:38:30 -0700

    1s

--- scripts/planets/wandura.js
+++ scripts/planets/wandura.js
@@ -1,4 +1,4 @@
-const wandura = extend(Planet, "wandura", Planets.sun, 2, 1.5, {
+const wandura = extend(Planet, "wandura", Planets.sun, 1, 1, {
   localizedName: "Wandura",
   generator: new SerpuloPlanetGenerator(),
   bloom: true,
