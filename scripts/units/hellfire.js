84b72a9b63324ee7ed31ded6b4bb3870eaf1df88
BlueWolf <61173114+BlueWolf3682@users.noreply.github.com>
2021-01-06 22:08:57 -0700

    test at making remove not function

--- scripts/units/hellfire.js
+++ scripts/units/hellfire.js
@@ -1,3 +1,6 @@
 const hellfire = extendContent(UnitType, "hellfire", {});
-hellfire.constructor = () => extend(UnitEntity, {});
+hellfire.constructor = () => extend(UnitEntity, {
+  removed(){
+    print("Cease.");
+}});
 hellfire.abilities.add(new ForceFieldAbility(300, 1450, 2546330, 25));
