c0e8ca9614e76874d7522f38c6acf7ec96fb7c1d
BlueWolf <61173114+BlueWolf3682@users.noreply.github.com>
2021-01-07 07:13:27 -0700

    hopefully fully fixate kill reasons

--- scripts/units/hellfire.js
+++ scripts/units/hellfire.js
@@ -2,6 +2,7 @@ const hellfire = extendContent(UnitType, "hellfire", {});
 hellfire.constructor = () => extend(UnitEntity, {
   killed(){
     if(this.health <= 0) this.super$killed();
+    else this.dead = false;
   },
   destroy(){
     if(this.dead) this.super$destroy();
