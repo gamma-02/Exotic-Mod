fb05f52348a936f374041bc6f29315c2dd75cbf0
BlueWolf <61173114+BlueWolf3682@users.noreply.github.com>
2021-01-08 14:16:59 -0700

    done?

--- scripts/planets/wandura.js
+++ scripts/planets/wandura.js
@@ -1,6 +1,8 @@
+var magnetizedSlag = Vars.content.getByName(ContentType.block, "exotic-mod-deep-mag-slag");
+var basaltMagnetizedSlag = Vars.content.getByName(ContentType.block, "exotic-mod-basalt-mag-slag");
+
 const wandura = extend(Planet, "wandura", Planets.sun, 4, 1, {
   localizedName: "Wandura",
-  generator: new SerpuloPlanetGenerator(),
   bloom: true,
   radius: 1,
   accessible: true, 
@@ -10,8 +12,30 @@ const wandura = extend(Planet, "wandura", Planets.sun, 4, 1, {
   atmosphereRadOut: 0.5,
   startSector: 15
 });
-wandura.meshLoader = () => extend(HexMesh, wandura, 8, {});
-
+wandura.meshLoader = () => extend(HexMesh, this, 8, {});
+wandura.generator = () => extend(SerpuloPlanetGenerator, {
+  rid: extend(arc.util.noise.RidgedPerlin, 1, 2{}),
+  scl: 5,
+  waterOffset: 0.07,
+  arr: [
+    [magnetizedSlag, basaltMagnetizedSlag, Blocks.basalt, Blocks.basalt, Blocks.basalt, Blocks.darksand, Blocks.darksand, Blocks.darksand, Blocks.darksand, basaltMagnetizedSlag, Blocks.stone, Blocks.stone],
+    [magnetizedSlag, basaltMagnetizedSlag, Blocks.basalt, Blocks.basalt, Blocks.darksand, Blocks.darksand, Blocks.darksand, Blocks.darksand, Blocks.darksand, basaltMagnetizedSlag, Blocks.stone, Blocks.stone, Blocks.stone],
+    [magnetizedSlag, basaltMagnetizedSlag, Blocks.basalt, Blocks.darksand, Blocks.char, Blocks.darksand, Blocks.darksand, Blocks.darksand, Blocks.darksand, basaltMagnetizedSlag, Blocks.stone, Blocks.stone, Blocks.stone],
+    [magnetizedSlag, basaltMagnetizedSlag, Blocks.darksand, Blocks.char, Blocks.char, Blocks.char, Blocks.darksand, Blocks.stone, Blocks.stone, Blocks.stone, Blocks.sand, Blocks.grass, Blocks.grass],
+    [magnetizedSlag, magnetizedSlag, basaltMagnetizedSlag, Blocks.darksand, Blocks.char, Blocks.darksand, Blocks.darksand, Blocks.basalt, Blocks.sand, Blocks.sand, Blocks.sand, Blocks.sand, Blocks.grass],
+    [magnetizedSlag, magnetizedSlag, basaltMagnetizedSlag, Blocks.darksand, Blocks.darksand, Blocks.darksand, Blocks.grass, Blocks.grass, Blocks.sand, Blocks.sand, Blocks.grass, Blocks.sand, Blocks.grass],
+    [magnetizedSlag, basaltMagnetizedSlag, Blocks.darksand, Blocks.darksand, Blocks.grass, Blocks.grass, Blocks.sand, Blocks.basalt, Blocks.basalt, Blocks.basalt, Blocks.grass, Blocks.sand, Blocks.grass],
+    [Blocks.taintedWater, Blocks.darksandTaintedWater, Blocks.basalt, Blocks.basalt, Blocks.basalt, Blocks.grass, Blocks.basalt, Blocks.slag, Blocks.basalt, Blocks.grass, Blocks.sand, Blocks.grass, Blocks.grass],
+    [basaltMagnetizedSlag, Blocks.basalt, Blocks.basalt, Blocks.basalt, Blocks.grass, Blocks.grass, Blocks.sand, Blocks.basalt, Blocks.basalt, Blocks.grass, Blocks.sand, Blocks.grass, Blocks.grass],
+    [basaltMagnetizedSlag, Blocks.basalt, Blocks.basalt, Blocks.grass, Blocks.grass, Blocks.grass, Blocks.sand, Blocks.sand, Blocks.sand, Blocks.sand, Blocks.grass, Blocks.grass, Blocks.grass], 
+    [Blocks.taintedWater, Blocks.darksandTaintedWater, Blocks.basalt, Blocks.grass, Blocks.grass, Blocks.grass, Blocks.grass, Blocks.sand, Blocks.sand, Blocks.grass, Blocks.grass, Blocks.grass, Blocks.grass],
+    [Blocks.darksandTaintedWater, Blocks.darksandTaintedWater, Blocks.basalt, Blocks.grass, Blocks.grass, Blocks.grass, Blocks.grass, Blocks.sand, Blocks.grass, Blocks.grass, Blocks.grass, Blocks.grass, Blocks.grass],
+    [basaltMagnetizedSlag, Blocks.basalt, Blocks.sand, Blocks.grass, Blocks.grass, Blocks.sand, Blocks.grass, Blocks.sand, Blocks.sand, Blocks.sand, Blocks.grass, Blocks.grass, Blocks.grass, Blocks.grass, Blocks.grass]
+  ],
+  dec: ObjectMap.of(),
+  tar: ObjectMap.of(),
+  water: 2 / 13
+});
 var lastStand = extend(SectorPreset, "last-stand", wandura, 69, {
   localizedName: "Last Stand",
   difficulty: 25,
