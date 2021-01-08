36ef8ca22477fcce317b36ed1870130f4978d3eb
BlueWolf <61173114+BlueWolf3682@users.noreply.github.com>
2021-01-07 20:04:42 -0700

    Update main.js

--- scripts/main.js
+++ scripts/main.js
@@ -1,6 +1,5 @@
 require("units/hellfire");
 require("planets/wandura");
-require("planets/planetGen");
 
 var lastStand = extend(SectorPreset, "last-stand", Planets.serpulo, 69, {
   localizedName: "Last Stand",
